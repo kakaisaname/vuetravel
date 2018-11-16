dc086ae7a901abeb8a56e8a3a909694c1bc5d7a1
caoyi <1198560793@qq.com>
2018-11-16 17:47:29 +0800

    '请求数据优化'

--- src/views/Index.vue
+++ src/views/Index.vue
@@ -14,6 +14,7 @@ import IndexIcons from '../components/Icons'
 import IndexRecommend from '../components/Recommend'
 import IndexWeekend from '../components/Weekend'
 import axios from 'axios'
+import { mapState } from 'vuex'
 
 export default {
     name:'Index',
@@ -30,11 +31,15 @@ export default {
             iconList:[],
             recommendList:[],
             weekendList:[],
+            lastCity: ''
         }
+    },
+    computed: {
+        ...mapState(['city'])
     },
     methods: {
         getIndexInfo () {
-            axios.get('/api/index.json').then(this.getIndexInfoSucc)
+            axios.get('/api/index.json?city=' + this.city).then(this.getIndexInfoSucc)
         },
         getIndexInfoSucc (res) {
             res = res.data
@@ -48,7 +53,14 @@ export default {
         }
     },
     mounted () {
+        lastCity = this.city
         this.getIndexInfo()
+    },
+    activated () {   //当使用了keep-alive 缓存路由后
+        if (this.lastCity !== this.city) {  //当城市改变了之后，重新发下ajax请求
+            this.lastCity = this.city
+            this.getIndexInfo()
+        }
     }
 }
 </script>
